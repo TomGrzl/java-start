142ab55ac19933403233ae3f8bc562f8c79a6c65
Tom Grehl <grehl.tom@googlemail.com>
2021-01-19 17:26:11 +0100

    initial commit

---
+++ src/AppMain.java
@@ -0,0 +1,15 @@
+public class AppMain {
+
+    public static void main(String[] args) {
+
+        int zahl = 0;
+        int andereZahl = 10;
+
+        String Summe = "Summe: " + (zahl + andereZahl);
+
+        System.out.print(Summe);
+
+
+
+    }
+}
